5bc5e6200292acf837cd80d0eac1116ebc445f18
PowerPack24 <iambruno77@gmail.com>
2024-10-13 20:49:24 -0400

    Completed the inband measurments using numpy, so we can process a few million samples without the need to wait very long. Small issues with timing, which is only visible when the program for 5-10 seconds. The timing issues should go away when running out of band measurements

--- inBandMeasurements/inband.py
+++ inBandMeasurements/inband.py
@@ -2,63 +2,123 @@ import sys
 import os
 import time
 import pdb
+from multiprocessing import Process
+import subprocess
+import pandas as pd
+from uprof.edittimechart import runner
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
 
+# adding the path to the global path 
 settingUpDaqPath = os.path.abspath("../settingUpDaq")
 if os.path.exists(settingUpDaqPath) and settingUpDaqPath not in sys.path:
     sys.path.append(settingUpDaqPath)
-
 from powerMeasurer import PowerPack
+from sklearn.model_selection import TimeSeriesSplit
 
-print(sys.path)
+# run AMDuProf function
+def runProfiler():
+    cmd = './profiler.sh'
+    subprocess.run(cmd)
 
-#print(os.getcwd())
 
-#pdb.set_trace()
-Power = PowerPack(numberOfSamplesToGather=100, rateOfSamples=1000, ohms=0.003)
+# Sampling rate
+samplingRate = 50000
+Power = PowerPack(numberOfSamplesToGather=2000, rateOfSamples=samplingRate, ohms=0.003)
 
-motherboard = [
-        ["cDAQ2Mod8/ai0","cDAQ2Mod8/ai1","cDAQ2Mod8/ai2","cDAQ2Mod8/ai3"],
-        ["cDAQ2Mod6/ai0", "cDAQ2Mod6/ai1", "cDAQ2Mod6/ai2", "cDAQ2Mod6/ai3", "cDAQ2Mod6/ai4"],
-        ["cDAQ2Mod2/ai17", "cDAQ2Mod2/ai18", "cDAQ2Mod2/ai19"]
-    ]
 
-gpu = [
-        [],
-        [],
-        ["cDAQ2Mod2/ai0","cDAQ2Mod2/ai1","cDAQ2Mod2/ai2"]
-    ]
-
+# We are only interested in the CPU currently
 cpu = [
         [],
         [],
         ["cDAQ2Mod2/ai4","cDAQ2Mod2/ai5","cDAQ2Mod2/ai6", "cDAQ2Mod2/ai7"]
     ]
 
-disk = [
-        ["cDAQ2Mod8/ai7"],
-        ["cDAQ2Mod6/ai7"],
-        ["cDAQ2Mod2/ai22"]
-    ]
-
-Power.initializePart("motherboard", motherboard)
-Power.initializePart("disk", disk)
+# Initialize the part
 Power.initializePart("cpu", cpu)
 
 
-start = time.time()
-# Start measurement task
+# Start measurement task for poth power pack and profiler
+profiler = Process(target=runProfiler)
 Power.start()
+profiler.start()
 
 
-# Simulate running for some time
-time.sleep(10)
+# match the length of the profiler, so we know for how to measure for
+time.sleep(5)
+
+# terminate the profiler and join the child process for synchronization
+profiler.terminate()
+profiler.join()
 
 # Stop measurement task
 Power.stop()
 
+# make the CSV
 Power.makeCSVs()
 
 
+# setting up a dataframe, and including a way too sum up all the wires
+powerPackDf = pd.read_csv('./csv/cpuMeasurements.csv')
+powerPackDf['voltage sum'] = powerPackDf.sum(axis=1)
 
 
-#./AMDuProfCLI  timechart --interval 100 --duration 10 --event socket=0,power -o /home/bruno//.AMDuProf/AMDuProf/
+# This is for taking the time from the front of the array
+times = np.arange(0, powerPackDf.shape[0]) / samplingRate
+powerPackDf['Times'] = times
+
+
+# cleaning the information inside main
+df = runner()
+
+# setting up the median window for times, voltage median, and voltage variance
+median_window = pd.DataFrame(columns=['times', 'voltage_median', 'voltage_var'])
+
+# Parameters, the loss is (window size + 1)
+window_size = 100
+voltage_sum = powerPackDf['voltage sum'].values
+times = powerPackDf['Times'].values
+
+# Create sliding windows using the strided trick
+voltage_windows = np.lib.stride_tricks.sliding_window_view(voltage_sum, window_shape=window_size)
+time_windows = np.lib.stride_tricks.sliding_window_view(times, window_shape=window_size)
+
+# Compute medians and variances for all windows at once
+voltage_medians = np.median(voltage_windows, axis=1)
+
+# Use the last time in each window for timing
+reference_times = time_windows[:, -1]
+
+# Create the DataFrame with the computed values
+median_window = pd.DataFrame({
+    'times': reference_times,
+    'voltage_median': voltage_medians
+})
+
+
+# plotting the data
+plt.rcParams["figure.figsize"] = (16,6)
+plt.rcParams['figure.dpi'] = 200
+plt.figure()
+
+# plotting the power pack data
+plt.plot(median_window['times'], median_window['voltage_median'], label='Window median', color = "red")
+
+# plotting the AMDuProf data
+plt.plot(df['time_in_seconds'], df['socket0-package-power'], label="Profiler", color ="blue")
+
+
+
+# Display the plot
+plt.xlabel('Time (seconds)')
+plt.ylabel('Power Consumption (Watts)')
+plt.title('Power Consumption of CPU Over Time')
+plt.legend()
+plt.grid(True)
+plt.savefig('./graphs/cpu.png')
+plt.show()
+
+
+
+
